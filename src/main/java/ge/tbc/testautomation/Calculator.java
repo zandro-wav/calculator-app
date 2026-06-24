b1cb179d0b3f5d4eb5a64dfdcfe83c96d5986f54
Sandro Revazishvili <revazishvili.sandro24@gtu.ge>
2026-06-24 23:40:56 +0400

    Initial commit

---
+++ src/main/java/ge/tbc/testautomation/Calculator.java
@@ -0,0 +1,9 @@
+package ge.tbc.testautomation;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Calculator App");
+        System.out.println("Addition: 5 + 3 = 8");
+        System.out.println("Subtraction: 5 - 3 = 2");
+    }
+}
